d2436c8d292b66bb8295e25d10d6b66e119120ab
Alice <lisgein47@gmail.com>
2015-02-19 01:04:21 +0300

    дополнение функции свап

---
+++ Stack/ConsoleApplication1/swap.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+
+class Stack
+{
+public:
+	Stack()
+		//std::string &current)
+		
+	{
+	}
+
+	~Stack()
+	{
+
+	}
+	void push(int value)//ввод элемента массива
+	{
+		a = value;
+	}
+	void swap(Stack &other)
+	{
+		std::swap(a,other.a);
+
+	}
+	void peek(){
+
+			std::cout << a << std::endl;
+	}
+
+private:
+	int a;
+};
+
+
+
+int main()
+{
+	Stack stack;
+	Stack left;
+	Stack right;
+	int value;
+	std::cout << "enter 1" << std::endl;
+		std::cin >> value;
+		left.push(value);
+	std::cout << "enter left.peek()" << std::endl;
+	left.peek();
+	std::cout << "enter peek" << std::endl;
+	right.peek();
+	std::cout << "swap..." << std::endl;
+	right.swap(left);
+	std::cout << "enter left.peek()" << std::endl;
+	left.peek();
+	std::cout << "enter peek" << std::endl;
+	right.peek();
+	std::cout << "enter 2" << std::endl;
+		std::cin >> value;
+		left.push(value);
+		std::cout << "enter left.peek()" << std::endl;
+		left.peek();
+		std::cout << "enter peek" << std::endl;
+		right.peek();
+	std::cout << "swap 2..." << std::endl;
+	std::cout << "enter left.peek()" << std::endl;
+	left.peek();
+	std::cout << "enter peek" << std::endl;
+	right.peek();
+	std::cout << "end" << std::endl;
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
---
+++ Stack/Swap/Swap.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+
+class Stack
+{
+public:
+	Stack()
+		:head_(0),
+		capacity_(3)
+	{
+	}
+
+	~Stack()
+	{
+		
+	}
+	void push(std::string value)//ввод элемента массива
+	{
+		head_++;
+		stack_[head_] = value;
+	}
+	void swap(Stack &other)
+	{
+		std::swap(head_, other.head_);
+	}
+	void peek(){
+		for (int i = 0; i < capacity_; i++)
+		{
+			std::cout << stack_[i] << std::endl;
+		}
+	}
+	
+private:
+	int head_;
+	int capacity_;
+	std::string *stack_ = new std::string[capacity_];
+};
+
+
+
+int main()
+{
+	Stack stack;
+	Stack left;
+	Stack right;
+	Stack &current = left;
+	std::string value;
+	std::cout << "1" << std::endl;
+	for (int i = 0; i < 3; i++){
+		std::cin >> value;
+		stack.push(value);
+	}
+	stack.peek();
+	std::cout << "swap:" << std::endl;
+	right.swap(left);
+	std::cout << "2" << std::endl;
+	stack.peek();
+	for (int i = 0; i < 3; i++){
+		std::cin >> value;
+		stack.push(value);
+	}
+	std::cout << "3" << std::endl;
+	stack.peek();
+
+
+	
+	
+	
+	system("pause"); 
+	return 0;
+}
