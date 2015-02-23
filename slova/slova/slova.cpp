530bd83fc820dd943d77a6a6415001e4da4187e0
Alice <lisgein47@gmail.com>
2015-02-24 02:23:29 +0300

    бедная програмка по подсчету букв в файле

--- slova/slova/slova.cpp
+++ slova/slova/slova.cpp
@@ -1,18 +1,30 @@
-#include <string>
+﻿#include <string>
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cctype>
 
 int main()
 {
-	std::vector <std::string> words;
 	std::ifstream in("C:\\Users\\LisGein\\Documents\\Beavis and Butt Head.txt");
-	std::string word;
-	while (in >> word)
+	std::string line;
+	std::vector<int> count(256, 0);
+
+	while (std::getline(in, line))
 	{
-		words.push_back(word);
+		for (int i = 0; i < line.size(); ++i)
+		{
+			if (isalpha(line[i]))
+			{
+				char current = line[i];
+				count[current] += 1;
+			}				
+		}
 	}
-	std::cout << words.size() << std::endl;
+	for (int i = 'a'; i <= 'z'; ++i)
+	{
+		std::cout << static_cast<char>(i)<< " - " << count[i] << std::endl;
+	}
 	system("pause");
 	return 0;
 }
