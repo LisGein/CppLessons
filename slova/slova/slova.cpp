05329763209ebe5aadffc73b1eccedf547053329
Alice <lisgein47@gmail.com>
2015-02-24 03:32:38 +0300

    уменьшение расхода памяти, вывод только испольующихся элементов, вывод максимального элемента

--- slova/slova/slova.cpp
+++ slova/slova/slova.cpp
@@ -8,23 +8,36 @@ int main()
 {
 	std::ifstream in("C:\\Users\\LisGein\\Documents\\Beavis and Butt Head.txt");
 	std::string line;
-	std::vector<int> count(256, 0);
-
+	std::vector<int> count(27, 0);
+	int x = 0;
+	char z;
+	int y = 0;
 	while (std::getline(in, line))
 	{
 		for (int i = 0; i < line.size(); ++i)
 		{
 			if (isalpha(line[i]))
 			{
-				char current = line[i];
-				count[current] += 1;
+				
+				char current = (char)tolower(line[i]);
+				y = static_cast<int>(current)- 96;
+				count[y] += 1;
 			}				
 		}
 	}
-	for (int i = 'a'; i <= 'z'; ++i)
+	for (int i = 0; i <= 26; ++i)
 	{
-		std::cout << static_cast<char>(i)<< " - " << count[i] << std::endl;
+		if (count[i] > 0){
+			y = i + 96;
+			std::cout << static_cast<char> (y) << " - " << count[i] << std::endl;
+		}
+		if (count[i] > x)
+		{
+			z = static_cast<char>(i);
+			x = count[i];
+		}
 	}
+	std::cout << "The greatest number of repetition letters: " << z << " - " << x << " times." << std::endl;
 	system("pause");
 	return 0;
 }
