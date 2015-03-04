dcacd970bfaed824f9614548c8719a43757c747e
Alice <lisgein47@gmail.com>
2015-03-04 04:34:07 +0300

    работающие круги, прямоугольники

--- SFML/SFML/main.cpp
+++ SFML/SFML/main.cpp
@@ -19,29 +19,46 @@ public:
 
 
 class Rectangle  //класс - прямоугольник
-	//:public MovingShape
+	:public MovingShape
 {
 public:
-	Rectangle(sf::Vector2f const &pos, sf::Vector2f const &size, sf::Color const &color, sf::Vector2f const &speed)
+	Rectangle(sf::Vector2f const &pos, sf::Vector2f const &speed, sf::Vector2f const &size, sf::Color const &color)
 		:speed_(speed), 
 		rect_(size),
 		size_(size),
 		pos_(pos)
+
 	{
 		rect_.setPosition(pos);
 		rect_.setFillColor(color);
 		
 	};
-	
-	void _draw(sf::RenderWindow &window)
+	void update(float dt)
+	{
+		if (pos_.y > WINDOW_HEIGHT - size_.y)
+			speed_ = sf::Vector2f(speed_.x, -abs(speed_.y));//пол
+
+		if (pos_.x > WINDOW_WIDTH - size_.x)
+			speed_ = sf::Vector2f(-abs(speed_.x), speed_.y);//право
+
+		if (pos_.y < 0) //потолок
+			speed_ = sf::Vector2f(speed_.x, abs(speed_.y));
+
+		if (pos_.x < 0) //лево
+			speed_ = sf::Vector2f(abs(speed_.x), speed_.y);
+
+		pos_ += speed_ * dt;
+		rect_.setPosition(pos_);
+	}
+	void draw(sf::RenderWindow &window)
 	{
 		window.draw(rect_);
 	}
 
 private:
 	sf::RectangleShape rect_;
-	sf::Vector2i pos_;
-	sf::Vector2i size_;
+	sf::Vector2f pos_;
+	sf::Vector2f size_;
 	sf::Vector2f speed_;
 };
 
@@ -88,52 +105,38 @@ private:
 	sf::Vector2f pos_;
 	float size_;
 };
-void init_rect(std::vector<Rectangle> &rect_, std::mt19937 &gen)  //определение всех переменных для прямоугольников
-{
-	std::uniform_real_distribution<float> rect_dist_x(40, 80);
-	std::uniform_real_distribution<float> rect_dist_y(20, 40);
-	std::uniform_int_distribution<> color_dist(0, 255);
-	std::uniform_real_distribution<float> x_coor(0.f, WINDOW_WIDTH);
-	std::uniform_real_distribution<float> sp_dist(-100, 100);
-	for (int i = 0; i < 30; i++)
-	{
-		sf::Color color(color_dist(gen), color_dist(gen), color_dist(gen));
-		sf::Vector2f pos(x_coor(gen), x_coor(gen));
-		sf::Vector2f size(rect_dist_x(gen), rect_dist_y(gen));
-		sf::Vector2f speed(sp_dist(gen), sp_dist(gen));
-		rect_.push_back(Rectangle(pos, size, color, speed));
-	}
-}
-void draw_rect(std::vector<Rectangle> &rect_)
-{
-	for (int i = 0; i < rect_.size(); ++i)
-	{
-		rect_[i]._draw(window);
-	}
-}
-void init_shape(std::vector <MovingShape *> shapes, std::mt19937 &gen)  //определение всех переменных для мячей
+
+void init_all(std::vector <MovingShape *> &shapes, std::mt19937 &gen)  //определение всех переменных для мячей
 {
 	std::uniform_int_distribution<> color_dist(0, 255);
 	std::uniform_real_distribution<float> sp_dist(-100, 100);
 
 	for (int i = 0; i < 30; i++)
 	{
-		
-		std::uniform_real_distribution<float> size_dist(5, 10);
+		//общие:
+		sf::Vector2f speed(sp_dist(gen), sp_dist(gen));
+		sf::Color color(color_dist(gen), color_dist(gen), color_dist(gen));
+		//для шаров:
+		std::uniform_real_distribution<float> size_dist(5, 10);  
 		float size = size_dist(gen);
 		std::uniform_real_distribution<float> x_dist(0.f, WINDOW_WIDTH - 2 * size);
 		std::uniform_real_distribution<float> y_dist(0.f, WINDOW_HEIGHT - 2 * size);
 		sf::Vector2f pos(x_dist(gen), y_dist(gen));
-		sf::Vector2f speed(sp_dist(gen), sp_dist(gen));
-		sf::Color color(color_dist(gen), color_dist(gen), color_dist(gen));
-		//MovingShape * shape = new Ball(pos, speed, size, color);
-		//circle_.push_back(Ball(pos, speed, size, color));
+		//для прямоугольников:
+		std::uniform_real_distribution<float> rect_dist_x(40, 80);
+		std::uniform_real_distribution<float> rect_dist_y(20, 40);
+		sf::Vector2f size_(rect_dist_x(gen), rect_dist_y(gen));
+		std::uniform_real_distribution<float> x_coor(0.f, WINDOW_WIDTH - size_.x);
+		std::uniform_real_distribution<float> y_coor(0.f, WINDOW_HEIGHT - size_.y);
+		sf::Vector2f pos_(x_coor(gen), y_coor(gen));
+
 		shapes.push_back(new Ball(pos, speed, size, color));
+		shapes.push_back(new Rectangle(pos_, speed, size_, color));
 
 
 	}
 }
-void draw_circle(std::vector <MovingShape *> shapes)
+void draw_all(std::vector <MovingShape *> shapes)
 {
 	
 	for (int i = 0; i < shapes.size(); ++i)
@@ -160,8 +163,7 @@ int main()
 	sf::Clock clock;
 	std::vector<Ball> circle_;
 	std::mt19937 gen(rd());
-	init_rect(rect_, gen);
-	init_shape(shapes, gen);
+	init_all(shapes, gen);
 
 	float last_up = clock.getElapsedTime().asSeconds();
 
@@ -175,9 +177,7 @@ int main()
 		}
 		update_all(last_up, shapes, clock);
 		window.clear();
-		draw_rect(rect_);
-
-		draw_circle(shapes);
+		draw_all(shapes);
 		window.display();
 	}
 	for (int i = 0; i < shapes.size(); ++i)
