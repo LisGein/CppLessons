aa5350f756c1957351ec55a66d0836bafdc09a21
Alice <lisgein47@gmail.com>
2015-03-13 05:42:41 +0300

    исправила баг

--- Cipher/Numbers/main.cpp
+++ Cipher/Numbers/main.cpp
@@ -6,15 +6,20 @@ char iput(char &number)
 {
 	int current;
 	if (isalnum(number))
-	{		
+	{
 		if ((number > 47) && (number < 58))
 			current = number - 48;
-		if ((number >= 'A') && (number <= 'F'))
+		else if ((number >= 'A') && (number <= 'F'))
 			current = number - 'A' + 10;
-		if ((number >= 'a') && (number <= 'f'))
+		else if ((number >= 'a') && (number <= 'f'))
 			current = number - 'a' + 10;
 		else
-			current = 0;
+		{
+			std::cout << "Error!";
+			system("pause");
+			current = 16;
+		}
+			
 		return current;
 	}
 }
@@ -33,17 +38,13 @@ int main()
 	std::getline(std::cin, input_num);
 	std::vector<int> o_nums;
 	for (int i = 0; i < input_num.size(); ++i)
-		if (iput(input_num[i]) != 0)
-		{
+	{
+		if (iput(input_num[i]) != 16)
 			o_nums.push_back(iput(input_num[i]));
-			degree += o_nums.size();
-		}
 		else
-		{
-			std::cout << "Error!";
-			system("pause");
 			return 0;
-		}
+	}
+	degree += o_nums.size();
 	for (int i = 0; i < o_nums.size(); ++i)
 	{
 		otv += (conv(o_nums[i], degree));
