3b1f2ddcc5f982633c3a26b1355d544bb20608d2
Alice <lisgein47@gmail.com>
2015-03-20 18:59:36 +0300

    Корректное считывание символов

--- Cipher/OCR/Main.cpp
+++ Cipher/OCR/Main.cpp
@@ -9,36 +9,55 @@
 int main()
 {
 	std::ifstream in("input.txt");
-	char character;
-	std::string test;
+	std::string recombination;
+	std::string line;
 	std::string combination;
 	int amount_char = 0;
 	std::set<std::string> comb_char;
-	std::set<std::string>::iterator it;
-	while (std::getline(in, test))
+	while (std::getline(in, line))
 	{
-		for (int i = 0; i < test.size(); ++i)
+		for (int i = 0; i < line.size(); ++i)
 		{
-			if (std::isalpha(test[i]))
+			bool ok_comb = true;
+			if (std::isalpha((unsigned char)line[i]))
 			{
-				combination += (char)tolower(test[i]);
+				combination += (char)tolower(line[i]);
 				amount_char++;
 			}
-			if (std::isspace(test[i]))
+			if (std::isspace((unsigned char)line[i]))
 			{
 				combination += ' ';
 				amount_char++;
 			}
-			if (amount_char % 3 == 0)
+			if ((!isalpha((unsigned char)line[i])) && (!isspace((unsigned char)line[i])))
+			{
+				ok_comb = false;
+				break;
+			}
+			if ((amount_char >= 3) && (combination.size() != 0))
 			{
 				comb_char.insert(combination);
-				combination.clear();
+				if (combination[combination.size() - 1] != ' ')
+				{
+					for (int i = 1; i < combination.size(); ++i)
+						recombination += combination[i];
+					combination.clear();
+					combination += recombination;
+					recombination.clear();
+				}
+				else 
+				{
+					amount_char = 1;
+					combination.clear();
+					combination += ' ';
+				}
 			}
 		}
 	}
-	for (it = comb_char.begin(); it != comb_char.end(); ++it)
-		std::cout <<  *it << "\n";
-	std::cout << combination;
+	std::set<std::string>::iterator it;
+ 	for (it = comb_char.begin(); it != comb_char.end(); ++it)
+ 		std::cout <<  *it << "\n";
+	
 	system("pause");
 	return 0;
 }
