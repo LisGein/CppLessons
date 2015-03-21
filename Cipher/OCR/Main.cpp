35f565f2d6298f27f1a3dce85b4859f0acc902e3
Alice <lisgein47@gmail.com>
2015-03-21 04:34:37 +0300

    Исправления

--- Cipher/OCR/Main.cpp
+++ Cipher/OCR/Main.cpp
@@ -9,21 +9,16 @@
 typedef std::pair<int, int> list_t;
 const int alphabet = 26;
 
-std::set<std::string> comb_set(std::string &line, int &shift)
+void comb_set(std::string &line, int &shift, std::set<std::string> &text)
 {
-	std::set<std::string> comb_char;
-	comb_char.clear();
 	std::string combination;
 	int amount_char = 0;
-	int circle;
-	int alp_in_numb;
 	for (int i = 0; i < line.size(); ++i)
 	{
 		if (std::isalpha((unsigned char)line[i]))
 		{
-			alp_in_numb = (char)tolower(line[i]) - 97;
-			circle = (alp_in_numb + shift) / alphabet;
-			combination += alp_in_numb + shift - (circle*alphabet) + 97;
+			int alp_in_numb = (char)tolower(line[i]) - 'a';
+			combination += (alp_in_numb + shift) % alphabet + 'a';
 			amount_char++;
 		}
 		if (std::isspace((unsigned char)line[i]))
@@ -38,7 +33,7 @@ std::set<std::string> comb_set(std::string &line, int &shift)
 		}
 		if ((amount_char >= 3) && (combination.size() != 0))
 		{
-			comb_char.insert(combination);
+			text.insert(combination);
 			if (combination[combination.size() - 1] != ' ')
 				combination = combination.substr(1);
 			else
@@ -48,8 +43,7 @@ std::set<std::string> comb_set(std::string &line, int &shift)
 				combination += ' ';
 			}
 		}
-	}
-	return comb_char;
+	}	
 };
 
 int best_res(std::set<std::string> &lib, std::set<std::string> &comb_text)
@@ -67,17 +61,14 @@ void reciphers(int &shift)
 	std::ifstream check("check.txt");
 	std::string recipher;
 	std::string word;
-	int circle;
-	int alp_in_numb;
 	while (std::getline(check, word))
 	{
 		for (int i = 0; i <= word.size(); ++i)
 		{
 			if (std::isalpha((unsigned char)word[i]))
 			{
-				alp_in_numb = (char)tolower(word[i]) - 97;
-				circle = (alp_in_numb + shift) / alphabet;
-				recipher += (alp_in_numb + shift) - (circle*alphabet) + 97;
+				int alp_in_numb = (char)tolower(word[i]) - 'a';
+				recipher += (alp_in_numb + shift) % alphabet + 'a';
 			}
 
 			if (std::isspace((unsigned char)word[i]))
@@ -89,23 +80,26 @@ void reciphers(int &shift)
 
 int main()
 {
-	std::set<std::string> lib;
 	std::ifstream in("input.txt");
 	std::string line;
+	std::set<std::string> lib;	
 	int shift = 0;
 	while (std::getline(in, line))
-		lib = comb_set(line, shift);
+		comb_set(line, shift, lib);
 
 	std::vector<int> res;
-	std::string words;
-	for (int shift = 0; shift < alphabet; ++shift)
+	std::string crypted_text;
+	std::string crypted_line;
+	std::ifstream check("check.txt");
+	while (std::getline(check, crypted_line))
 	{
-		std::ifstream check("check.txt");
-		std::set<std::string> comb_text;
-		while (std::getline(check, words))
-			comb_text = comb_set(words, shift);
-		res.push_back(best_res(lib, comb_text));
-		check.close();
+		crypted_text += crypted_line;
+		for (int shift = 0; shift < alphabet; ++shift)
+		{
+			std::set<std::string> comb_text;
+			comb_set(crypted_line, shift, comb_text);
+			res.push_back(best_res(lib, comb_text));
+		}
 	}
 	shift = 0;
 	for (int i = 0; i < res.size(); ++i)
