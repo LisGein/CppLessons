618a1278318147945cc513895f2257153f578e8c
Alice <lisgein47@gmail.com>
2015-03-24 01:47:37 +0300

    Исправлено оформление.

--- A+B/ConsoleApplication1/Area_M-N/Main.cpp
+++ A+B/ConsoleApplication1/Area_M-N/Main.cpp
@@ -5,11 +5,13 @@
 #include <Random>
 
 typedef std::pair<int, int> list_int;
-
+typedef std::pair<float, float> list_f;
 
 std::vector<std::pair<float, float>> set_points(int &global_area, int min_x, int min_y, int max_x, int max_y)
 {
-	int num_rd = global_area * global_area;
+	int num_rd = global_area;
+	if (global_area < 1000)
+		num_rd *= global_area;
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::vector<std::pair<float, float>> points(num_rd);
@@ -23,79 +25,34 @@ std::vector<std::pair<float, float>> set_points(int &global_area, int min_x, int
 	return points;
 };
 
-void inside(int &intersect_count, int &k)
-{
-	if (intersect_count % 2 != 0)
-		k++;
-};
 void area_calc(int &global_area, int &k, int &n)
 {
 	double area = global_area * k / n;
 	std::cout << area;
 };
 
-void intersect_diag(int &intersect_count, int &A_x, int &A_y, int &B_x, int &B_y, float &X_x, float &X_y, int &min_y)
+int intersect(std::pair<float, float> &X_coor, list_int &A_coor,list_int &B_coor, int &min, int &intersect_count)
 {
-	double A = A_y - B_y;
-	double B = B_x - A_x;
-	double C = ((A_x * B_y) - (B_x * A_y));
-	double D = (X_y - min_y);
-	double E = (X_x * min_y) - (X_x * X_y);
-	double y = ((((A * E) / D) - C) / B);
-	if ((y <= A_y) || (y <= B_y))
-		if (y <= X_y)
-			intersect_count++;
-};
-
-void intersect(std::vector<std::pair<float, float>> &coor_points, std::vector<list_int> &angles, int &min_y, int &global_area)
-{
-	int n = 0;
-	int k = 0;
-
-	for (int i = 0; i < coor_points.size(); ++i)
-	{
-		n++;
-		int intersect_count = 0;
-		float X_x = coor_points[i].first;
-		float X_y = coor_points[i].second;
-		for (int coor = 0; coor < angles.size(); ++coor)
+	if (((A_coor.first < X_coor.first) && (B_coor.first >= X_coor.first)) || ((A_coor.first > X_coor.first) && B_coor.first <= X_coor.first))//если А справа, а В слева или
 		{
-			int A_x = angles[coor].first;
-			int A_y = angles[coor].second;
-			if (coor != angles.size() - 1)
+			if ((A_coor.second <= X_coor.second) && (B_coor.second <= X_coor.second))//если прямая ниже точки
+				intersect_count++;
+			else if (((A_coor.second > X_coor.second) && (B_coor.second <= X_coor.second)) || ((A_coor.second < X_coor.second) && (B_coor.second >= X_coor.second)))//если А выше, а В ниже или В выше, а А ниже
 			{
-				int B_x = angles[coor + 1].first;
-				int B_y = angles[coor + 1].second;
-				if (((A_x < X_x) && (B_x >= X_x)) || ((A_x > X_x) && (B_x <= X_x)))//если А справа, а В слева или
-				{
-					if ((A_y <= X_y) && (B_y <= X_y))//если прямая ниже точки
+				double A = A_coor.second - B_coor.second;
+				double B = B_coor.first - A_coor.first;
+				double C = ((A_coor.first * B_coor.second) - (B_coor.first * A_coor.second));
+				double D = (X_coor.second - min);
+				double E = (X_coor.first * min) - (X_coor.first * X_coor.second);
+				double y = ((((A * E) / D) - C) / B);
+				if ((y <= A_coor.second) || (y <= B_coor.second))
+					if (y <= X_coor.second)
 						intersect_count++;
-					else if (((A_y > X_y) && (B_y <= X_y)) || ((A_y < X_y) && (B_y >= X_y)))//если А выше, а В ниже или В выше, а А ниже
-						intersect_diag(intersect_count, A_x, A_y, B_x, B_y, X_x, X_y, min_y);		
-				}
-				else if ((A_x == X_x) && (B_x == X_x))
-					intersect_count++;
-			}
-
-			else
-			{
-				int B_x = angles[0].first;
-				int B_y = angles[0].second;
-
-				if (((A_x < X_x) && (B_x >= X_x)) || ((A_x > X_x) && (B_x <= X_x)))//если А справа, а В слева или
-				{
-					if ((A_y <= X_y) && (B_y <= X_y))//если прямая ниже точки			
-						intersect_count++;
-					else if (((A_y > X_y) && (B_y <= X_y)) || ((A_y < X_y) && (B_y >= X_y)))//если А выше, а В ниже или В выше, а А ниже					
-						intersect_diag(intersect_count, A_x, A_y, B_x, B_y, X_x, X_y, min_y);					
-				}
-				else if ((A_x == X_x) && (B_x == X_x))
-					intersect_count++;
 			}
 		}
-		inside(intersect_count, k);
-	}
-	area_calc(global_area, k, n);
+	else if ((A_coor.first == X_coor.first) && (B_coor.first == X_coor.first))
+			intersect_count++;
+	return intersect_count;
 };
 
 int main()
@@ -131,8 +88,36 @@ int main()
 	max_y++;
 
 	int global_area = (max_x - min_x)* (max_y - min_y);
-	std::vector<std::pair<float, float>> coor_points = set_points(global_area, min_x, min_y, max_x, max_y);
-	intersect(coor_points, angles, min_y, global_area);
+	std::vector<list_f> coor_points = set_points(global_area, min_x, min_y, max_x, max_y);
+	list_f X_coor;
+	list_int A_coor;
+	list_int B_coor;
+	int n = 0;
+	int k = 0;
+
+	for (int i = 0; i < coor_points.size(); ++i)
+	{
+		n++;
+		int intersect_count = 0;
+		X_coor = std::make_pair(coor_points[i].first, coor_points[i].second);
+		for (int coor = 0; coor < angles.size(); ++coor)
+		{
+			A_coor = std::make_pair(angles[coor].first, angles[coor].second);
+			if (coor != angles.size() - 1)
+			{
+				B_coor = std::make_pair(angles[coor + 1].first, angles[coor + 1].second);
+				intersect(X_coor, A_coor, B_coor, min_y, intersect_count);
+			}
+			else
+			{
+				B_coor = std::make_pair(angles[0].first, angles[0].second);
+				intersect(X_coor, A_coor, B_coor, min_y, intersect_count);
+			}
+		}
+		if (intersect_count % 2 != 0)
+			k++;
+	}
+	area_calc(global_area, k, n);
 	system("pause");
 	return 0;
 }
