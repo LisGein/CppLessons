54a1b0d7efb5401b7fcc32e757edaa481cc90dec
Alice <lisgein47@gmail.com>
2015-03-23 21:59:57 +0300

    Изменена функция проверки пересечения с диагоналями.

--- A+B/ConsoleApplication1/Area_M-N/Main.cpp
+++ A+B/ConsoleApplication1/Area_M-N/Main.cpp
@@ -23,7 +23,31 @@ std::vector<std::pair<float, float>> set_points(int &global_area, int min_x, int
 	return points;
 };
 
-void area_calc(std::vector<std::pair<float, float>> &coor_points, std::vector<list_int> &angles, int &min_y, int &global_area)
+void inside(int &intersect_count, int &k)
+{
+	if (intersect_count % 2 != 0)
+		k++;
+};
+void area_calc(int &global_area, int &k, int &n)
+{
+	double area = global_area * k / n;
+	std::cout << area;
+};
+
+void intersect_diag(int &intersect_count, int &A_x, int &A_y, int &B_x, int &B_y, float &X_x, float &X_y, int &min_y)
+{
+	double A = A_y - B_y;
+	double B = B_x - A_x;
+	double C = ((A_x * B_y) - (B_x * A_y));
+	double D = (X_y - min_y);
+	double E = (X_x * min_y) - (X_x * X_y);
+	double y = ((((A * E) / D) - C) / B);
+	if ((y <= A_y) || (y <= B_y))
+		if (y <= X_y)
+			intersect_count++;
+};
+
+void intersect(std::vector<std::pair<float, float>> &coor_points, std::vector<list_int> &angles, int &min_y, int &global_area)
 {
 	int n = 0;
 	int k = 0;
@@ -31,7 +55,7 @@ void area_calc(std::vector<std::pair<float, float>> &coor_points, std::vector<li
 	for (int i = 0; i < coor_points.size(); ++i)
 	{
 		n++;
-		int bool_k = 0;
+		int intersect_count = 0;
 		float X_x = coor_points[i].first;
 		float X_y = coor_points[i].second;
 		for (int coor = 0; coor < angles.size(); ++coor)
@@ -44,20 +68,13 @@ void area_calc(std::vector<std::pair<float, float>> &coor_points, std::vector<li
 				int B_y = angles[coor + 1].second;
 				if (((A_x < X_x) && (B_x >= X_x)) || ((A_x > X_x) && (B_x <= X_x)))//если А справа, а В слева или
 				{
-					if ((A_y < X_y) && (B_y <= X_y))//если прямая ниже точки
-						bool_k++;
+					if ((A_y <= X_y) && (B_y <= X_y))//если прямая ниже точки
+						intersect_count++;
 					else if (((A_y > X_y) && (B_y <= X_y)) || ((A_y < X_y) && (B_y >= X_y)))//если А выше, а В ниже или В выше, а А ниже
-					{
-						double A = A_y - B_y;
-						double B = B_x - A_x;
-						double C = ((A_x * B_y) - (B_x * A_y));
-						double D = X_y - min_y;
-						double E = (min_y - X_y) * X_x;
-						int y = (A * E / D - C) / B;
-						if ((y < A_y) || (y <= B_y))
-							bool_k++;
-					}
+						intersect_diag(intersect_count, A_x, A_y, B_x, B_y, X_x, X_y, min_y);		
 				}
+				else if ((A_x == X_x) && (B_x == X_x))
+					intersect_count++;
 			}
 
 			else
@@ -67,27 +84,18 @@ void area_calc(std::vector<std::pair<float, float>> &coor_points, std::vector<li
 
 				if (((A_x < X_x) && (B_x >= X_x)) || ((A_x > X_x) && (B_x <= X_x)))//если А справа, а В слева или
 				{
-					if ((A_y < X_y) && (B_y <= X_y))//если прямая ниже точки			
-						bool_k++;
-					else if (((A_y > X_y) && (B_y <= X_y)) || ((A_y < X_y) && (B_y >= X_y)))//если А выше, а В ниже или В выше, а А ниже
-					{
-						double A = A_y - B_y;
-						double B = B_x - A_x;
-						double C = ((A_x * B_y) - (B_x * A_y));
-						double D = X_y - min_y;
-						double E = (min_y - X_y) * X_x;
-						int y = (A * E / D - C) / B;
-						if ((y < A_y) || (y <= B_y))
-							bool_k++;
-					}
+					if ((A_y <= X_y) && (B_y <= X_y))//если прямая ниже точки			
+						intersect_count++;
+					else if (((A_y > X_y) && (B_y <= X_y)) || ((A_y < X_y) && (B_y >= X_y)))//если А выше, а В ниже или В выше, а А ниже					
+						intersect_diag(intersect_count, A_x, A_y, B_x, B_y, X_x, X_y, min_y);					
 				}
+				else if ((A_x == X_x) && (B_x == X_x))
+					intersect_count++;
 			}
 		}
-		if (bool_k % 2 != 0)
-			k++;
+		inside(intersect_count, k);
 	}
-	double area = global_area * k / n;
-	std::cout << area;
+	area_calc(global_area, k, n);
 };
 
 int main()
@@ -117,9 +125,14 @@ int main()
 		if (angles[i].second > max_y)
 			max_y = angles[i].second;
 	}
+	min_x--;
+	max_x++;
+	min_y--;
+	max_y++;
+
 	int global_area = (max_x - min_x)* (max_y - min_y);
 	std::vector<std::pair<float, float>> coor_points = set_points(global_area, min_x, min_y, max_x, max_y);
-	area_calc(coor_points, angles, min_y, global_area);
+	intersect(coor_points, angles, min_y, global_area);
 	system("pause");
 	return 0;
 }
