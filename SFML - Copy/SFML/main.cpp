edf95f2be5ca28bd182711db31f9f4da4b7e9930
Alice <lisgein47@gmail.com>
2015-03-27 12:02:50 +0300

    Исправленный вариант, но обход в ширину и ещё не доделала с max

--- SFML - Copy/SFML/main.cpp
+++ SFML - Copy/SFML/main.cpp
@@ -5,7 +5,6 @@
 #include <string>
 #include <utility>
 
-
 struct point_t
 {
 	point_t()
@@ -14,7 +13,7 @@ struct point_t
 	point_t(int x, int y)
 		:x(x), y(y)
 	{}
-	bool operator< (point_t &other)
+	bool operator< (point_t const &other) const
 	{
 		if (y < other.y)
 			return true;
@@ -27,20 +26,35 @@ struct point_t
 };
 typedef std::map<point_t, std::vector<point_t>> blocks_graph_t;
 
-void DFS(point_t current, blocks_graph_t &blocks_graph)
+void DFS(point_t current, blocks_graph_t &blocks_graph, std::set<point_t> &visited, std::vector<point_t> &max_x)
 {
-	std::set<point_t> visited;
 	visited.insert(current);
 	std::cout << "we are in (" << current.x << ", " << current.y << ")" << std::endl;
-	std::vector<point_t> max_x;
-	for (point_t const & next : blocks_graph[current]) // Для каждого ребра
+	for (auto iter = blocks_graph[current].begin(); iter != blocks_graph[current].end(); ++iter) // Для каждого ребра
 	{
+		point_t &next = *iter;
 		if (visited.find(next) == visited.end())
 		{
-			max_x.push_back(next);
-			DFS(next, blocks_graph); // Запускаемся из соседа
+			if (max_x[0].x < next.x)
+			{
+				max_x.clear();
+				max_x.push_back(next);
+			}
+			else if (max_x[0].x == next.x)
+			{
+				int find_max = next.y;
+				for (int i_y = 0; i_y < max_x.size(); ++i_y)
+				{
+					if (max_x[i_y].y < find_max)
+						find_max = max_x[i_y].y;;
+				}
+				if (find_max != next.y)
+				max_x.push_back(next);
+			}
+			DFS(next, blocks_graph, visited, max_x); // Запускаемся из соседа
 		}
 	}
+
 	for (int i = 0; i < max_x.size(); ++i)
 	std::cout << max_x[i].x << " - " << max_x[i].y << "\n";
 };
@@ -56,18 +70,50 @@ int main()
 	point_t pos(0,0);//первая точка
 	sf::Color color = code_hard.getPixel(0,0);
 	blocks_graph_t blocks_graph;//вершина и её ребра
-	for (int row = 0; row < WINDOW_WIDTH; ++row)
+	for (int col = 0; col < WINDOW_HEIGHT; ++col) 
 	{
-		for (int col = 0; col < WINDOW_HEIGHT - 1; ++col) //-1!!!!&!?!?!?!
+		if (col != WINDOW_HEIGHT - 1)
 		{
-			if (code_hard.getPixel(row, col) == code_hard.getPixel(row, col + 1))//если ячейки (row, col) и (row, col + 1) одного цвета
+			for (int row = 0; row < WINDOW_WIDTH; ++row)
 			{
-				blocks_graph[point_t(row, col)].push_back(point_t(row, col));
-				blocks_graph[point_t(row, col + 1)].push_back(point_t(row, col + 1));
+				if (row != WINDOW_WIDTH - 1)//если x != ширине
+				{
+					if (code_hard.getPixel(row, col) == code_hard.getPixel(row + 1, col))//если ячейки (row, col) и (row + 1, col) одного цвета
+					{
+						blocks_graph[point_t(row, col)].push_back(point_t(row, col));
+						blocks_graph[point_t(row, col)].push_back(point_t(row + 1, col));
+						if (code_hard.getPixel(row, col) == code_hard.getPixel(row, col + 1))
+							blocks_graph[point_t(row, col)].push_back(point_t(row, col + 1));
+
+					}
+					else if (code_hard.getPixel(row, col) == code_hard.getPixel(row, col + 1))
+					{
+						blocks_graph[point_t(row, col)].push_back(point_t(row, col));
+						blocks_graph[point_t(row, col)].push_back(point_t(row, col + 1));
+					}
+				}
+				else if (code_hard.getPixel(row, col) == code_hard.getPixel(row, col + 1))
+				{
+					blocks_graph[point_t(row, col)].push_back(point_t(row, col));
+					blocks_graph[point_t(row, col)].push_back(point_t(row, col + 1));
+				}
 			}
 		}
+		else
+		{
+			for (int row = 0; row < WINDOW_WIDTH; ++row)  
+				if (row != WINDOW_WIDTH - 1)
+					if (code_hard.getPixel(row, col) == code_hard.getPixel(row + 1, col))
+					{
+						blocks_graph[point_t(row, col)].push_back(point_t(row, col));
+						blocks_graph[point_t(row, col)].push_back(point_t(row + 1, col));
+					}			
+		}
 	}
-	DFS(pos, blocks_graph);
+	std::set<point_t> visited;
+	std::vector<point_t> max_x;
+	max_x.push_back(pos);
+	DFS(pos, blocks_graph, visited, max_x);
 
 	system("pause");
 	return 0;
